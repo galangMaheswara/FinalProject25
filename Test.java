8968750f8f79cf48491948a6f7d2d30d9684046b
hachuxyy <msamsularifinb6@gail.com>
2025-09-25 09:12:53 +0700

    Perubahan 1 yang dilakukan Haqi

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Halo, Ini Perubahan dilakukan oleh Haqi");
+        System.out.println("Ini yang edit Mas Haqi");
     }
 }
