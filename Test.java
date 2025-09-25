92cbd2e192cfeebf3cab07b864d1a30345129ebf
galangMaheswara <ullyses88@gmail.com>
2025-09-25 09:14:50 +0700

    perubahan 2 oleh galang

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Halo, Ini Perubahan dilakukan oleh Haqi");
         System.out.println("Ini yang edit Mas Haqi");
+        System.out.println("Ini yang edit paduka raja Galang");
     }
 }
